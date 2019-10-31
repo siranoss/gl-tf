856b18a04ff4da70ceca897d3b13d2b27ddfbdf0
ElMuchacho <basiledermigny@gmail.com>
2019-10-31 06:48:21 +0100

    passage des fonctions d'upload en typescript (niveau front-end), installation du bend multer et début du bend spring boot

--- angular-api/src/app/app.component.ts
+++ angular-api/src/app/app.component.ts
@@ -1,54 +1,60 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+
+const axios = require('axios');
+
+interface HTMLInputEvent extends Event {
+    target: HTMLInputElement & EventTarget;
+}
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'angular-api';
+  @ViewChild('pickerData',{static:false}) pickerData = ElementRef;
+  @ViewChild('pickerScript',{static:false}) pickerScript = ElementRef;
+  evtData : HTMLInputEvent;
+  evtScript : HTMLInputEvent;
 
-  uploadScript() {
-    /*let pickerScript = document.getElementById('pickerScript');
-    var evtScript;
-    pickerScript.addEventListener('change', e => {
-      for (let file of Array.from(e.target.files)) {
-        let item = document.createElement('li');
-        item.textContent = file.webkitRelativePath;
-      };
-      evtScript = e;
-    });
+  ngOnInit() {}
 
-    const data = new FormData();
-    for(var x = 0; x<evtScript.target.files.length; x++) {
-      data.append('file', evtScript.target.files[x]);
-    }
-    axios.post("Bend address", data, {
-    })
-    .then(res => {
-      console.log(res.statusText);
-    });*/
+  ngAfterViewInit() {}
+
+  onFileChangeData(event) {
+    this.evtData = event;
+  }
+
+  onFileChangeScript(event) {
+    this.evtScript = event;
   }
 
   uploadData() {
-    /*let pickerData = document.getElementById('pickerData');
-    var evtData;
-    pickerData.addEventListener('change', e => {
-      for (let file of Array.from(e.target.files)) {
-        let item = document.createElement('li');
-        item.textContent = file.webkitRelativePath;
-      };
-      evtData = e;
-    });
 
     const data = new FormData();
-    for(var x = 0; x<evtData.target.files.length; x++) {
-      data.append('file', evtData.target.files[x]);
+    for(var x = 0; x<this.evtData.target.files.length; x++) {
+      data.append('file', this.evtData.target.files[x]);
+      console.log(this.evtData.target.files[x]);
     }
-    axios.post("Bend address", data, {
+    axios.post("http://localhost:8000/upload", data, {
     })
     .then(res => {
       console.log(res.statusText);
-    });*/
+    });
+  }
+
+  uploadScript() {
+
+    const data = new FormData();
+    for(var x = 0; x<this.evtScript.target.files.length; x++) {
+      data.append('file', this.evtScript.target.files[x]);
+    }
+    axios.post("http://localhost:8000/upload", data, {
+    })
+    .then(res => {
+      console.log(res.statusText);
+    });
   }
 }
