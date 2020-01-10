c99a4754224e3c30661193c4e9ef3809558a4624
moi <leo.tisserand.17@gmail.com>
2020-01-10 12:48:20 +0100

    no more useless button or refresh

--- angular-api/src/app/app.component.ts
+++ angular-api/src/app/app.component.ts
@@ -17,12 +17,9 @@ export class AppComponent implements OnInit {
 
     @ViewChild('dataInput', { static: false }) dataInput: ElementRef;
     @ViewChild('scriptInput', { static: false }) scriptInput: ElementRef;
-    @ViewChild('importInput', { static: false }) importInput: ElementRef;
 
-    editorOptionsCode = { theme: 'vs-dark', language: 'python' };
-    editorOptionsImport = { theme: 'vs-dark', language: 'python' };
+    editorOptions = { theme: 'vs-dark', language: 'python' };
     code: string = 'def max(a, b):\n\tif (a > b):\n\t\treturn a\n\telse:\n\t\treturn b\n\nprint(max(1, 3))\n';
-    import: string = '';
 
     runIndication: String;
     resultValue: String;
@@ -31,7 +28,6 @@ export class AppComponent implements OnInit {
     errorHidder: boolean;
     dataUploader: FileUploader;
     scriptUploader: FileUploader;
-    importUploader: FileUploader;
     dataIsDropOver: boolean;
     scriptIsDropOver: boolean;
     products = [];
@@ -48,17 +44,7 @@ export class AppComponent implements OnInit {
         this.errorHidder = true;
         const headers = [{ name: 'Accept', value: 'application/json' }];
         this.dataUploader = new FileUploader({ url: 'api/data', autoUpload: true, headers: headers });
-        /*this.dataUploader.onCompleteAll = () => alert('File uploaded');*/
         this.scriptUploader = new FileUploader({ url: 'api/script', autoUpload: true, headers: headers });
-        this.importUploader = new FileUploader({ url: 'api/import', autoUpload: true, headers: headers });
-        /* this.scriptUploader.onCompleteAll = () => {
-             alert('File uploaded');
-         }*/
-        this.scriptUploader.response.subscribe(res => this.code = res);
-        this.importUploader.response.subscribe(res => {
-            this.import = res;//[0].importFile;
-            console.log(res);
-        });
     }
 
     displayer() {
@@ -80,10 +66,6 @@ export class AppComponent implements OnInit {
 
     scriptClicked() {
         this.scriptInput.nativeElement.click();
-    }
-
-    importClicked() {
-        this.importInput.nativeElement.click();
     }
 
     fillList() {
@@ -108,12 +90,12 @@ export class AppComponent implements OnInit {
             scriptName: script.file.name,
             dataList: this.filesToSend
         }
+
+        console.log(jsonToPost);
         this.listFiles = [];
         this.filesToSend = [];
         this.dataUploader.queue = [];
         this.scriptUploader.queue = [];
-
-        console.log(jsonToPost);
         this.runScriptGetRequest.post('http://localhost:8080/api/run', jsonToPost).subscribe((response) => {
             console.log(response);
 
@@ -136,11 +118,5 @@ export class AppComponent implements OnInit {
         let file = new File(this.code.split(""), "_pythonScript.py", { type: "text/plain" });
         let files = [file];
         this.scriptUploader.addToQueue(files);
-    }
-
-    uploadWrittenImport() {
-        let file = new File(this.import.split(""), "_pythonImport.py", { type: "text/plain" });
-        let files = [file];
-        this.importUploader.addToQueue(files);
     }
 }
